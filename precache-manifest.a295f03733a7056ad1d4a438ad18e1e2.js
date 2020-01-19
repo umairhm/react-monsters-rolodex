4cd31e1f19079da1289228e5a3c482a304734019
Umair Hafeez <umair.hafeez@rci.rogers.com>
2020-01-19 01:17:41 -0500

    Updates

---
+++ precache-manifest.a295f03733a7056ad1d4a438ad18e1e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df4c009b0c1a78e168c79418eec2ec9",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8cddbd72e1edb0050f9c",
+    "url": "/react-monsters-rolodex/static/css/main.b635b742.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/react-monsters-rolodex/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "8cddbd72e1edb0050f9c",
+    "url": "/react-monsters-rolodex/static/js/main.bb2909a9.chunk.js"
+  },
+  {
+    "revision": "09fc101723e82bb4a8f4",
+    "url": "/react-monsters-rolodex/static/js/runtime~main.2a38a34c.js"
+  }
+]);
